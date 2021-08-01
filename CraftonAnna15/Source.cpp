0d82b3abdf738ccc02692ef9021a7c14930dd414
AbruptEdge <acrafton11@gmail.com>
2021-08-01 12:12:46 -0400

    Add project files.

---
+++ CraftonAnna15/Source.cpp
@@ -0,0 +1,42 @@
+//Anna Crafton
+//CIS 1202 101
+//July 30 2021
+
+//CraftonAnna15
+
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+//functions
+int half(int);
+double half(double);
+float half(float);
+
+int manin() {
+
+
+
+}
+
+//halfs int, rounding up from .5.
+int half(int num) {
+	int half;
+	half = num / 2;
+	return half;
+};
+
+//halfs double.
+double half(double num) {
+	double half;
+	half = num / 2;
+	return half;
+};
+
+//halfs float.
+float half(float num) {
+	float half;
+	half = num / 2;
+	return half;
+}
